6b5ccab70508c29d5d91529061b086e224c511b8
Developerko <koshapatel2110@gmail.com>
2024-06-10 15:42:59 +0530

    Add New files

---
+++ Data/CouponStore.cs
@@ -0,0 +1,12 @@
+﻿using Web_Api_sam.Models;
+
+namespace Web_Api_sam.Data
+{
+    public static class CouponStore
+    {
+        public static List<coupon> couponList = new List<coupon> {
+            new coupon{id=1,name="10ff",percent=10,IaActive=true},
+            new coupon{id=2,name="20gh",percent=20,IaActive=false}
+        };
+    }
+}
